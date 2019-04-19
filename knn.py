1b875df68b4dffbf6d7dda1a53d13cdcd1a87adc
Aditya Mehta <aditya.mehta@knolskape.com>
2019-04-19 15:56:58 +0530

    dropped emp id. obtain train and test data now.

--- knn.py
+++ knn.py
@@ -1,8 +1,10 @@
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
-knn = KNeighborsClassifier(n_neighbors=1)
 
-data = pd.read_csv('')
+data = pd.read_csv('TESTdata.csv')
+
+data.drop(['Emp_ID'], axis=1)
 
 # get data csv file, segregate train and test data. do model coding later. 
+# knn = KNeighborsClassifier(n_neighbors=1)
 # knn.fit(X, y)
